425eee277d16f9fff9e2cc1ecf71f891473c3a85
Max <ma.abdulkerimov@gmail.com>
2024-03-14 04:21:01 +0100

    chore: added formatting to config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,28 +1,30 @@
 from pydantic import BaseModel
 
+
 # Shcema for the document
 # TODO - Add more fields (e.g. date, author, etc.)
 class Document(BaseModel):
-  title: str
-  content: str
+    title: str
+    content: str
+
 
 # Configuration file for elasticsearch
 es_config = {
-  "settings": {
-    "analysis": {
-      "analyzer": {
-        "my_analyzer": {
-          "type": "custom",
-          "tokenizer": "standard",
-          "filter": ["lowercase", "stemmer", "asciifolding"]
+    "settings": {
+        "analysis": {
+            "analyzer": {
+                "my_analyzer": {
+                    "type": "custom",
+                    "tokenizer": "standard",
+                    "filter": ["lowercase", "stemmer", "asciifolding"],
+                }
+            }
         }
-      }
-    }
-  },
-  "mappings": {
-    "properties": {
-        "title": {"type": "text", "analyzer": "my_analyzer"},
-        "content": {"type": "text", "analyzer": "english"}
-    }
-  }
+    },
+    "mappings": {
+        "properties": {
+            "title": {"type": "text", "analyzer": "my_analyzer"},
+            "content": {"type": "text", "analyzer": "english"},
+        }
+    },
 }
